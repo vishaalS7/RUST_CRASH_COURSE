8fe0e941b3d69bf7929ceb440f254856cf0e46ad
vishaalS7 <ivishaal1411@gmail.com>
2025-08-05 12:29:28 +0530

    HashMap

--- topics/if_else/exercises/src/lib.rs
+++ topics/if_else/exercises/src/lib.rs
@@ -1,11 +1,23 @@
 pub fn min(x: i32, y: i32) -> i32 {
-    todo!();
+    if x < y {
+        x
+    } else {
+        y
+    }
 }
 
 pub fn max(x: i32, y: i32) -> i32 {
-    todo!();
+    if x > y {
+        x
+    } else {
+        y
+    }
 }
 
 pub fn sign(x: i32) -> i32 {
-    todo!();
+    if x < 0 {
+        -1
+    } else {
+        1
+    }
 }
